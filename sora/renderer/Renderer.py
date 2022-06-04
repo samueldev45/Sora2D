21836f8bf38e49707f228c0304200eb5ec90b231
samueldev45 <76446373+samueldev45@users.noreply.github.com>
2022-06-04 20:14:13 -0300

    New renderer implemented

--- sora/renderer/Renderer.py
+++ sora/renderer/Renderer.py
@@ -9,19 +9,23 @@ class Renderer:
         surface.fill(scene.backgroundColor)
         actors = [obj for obj in scene.getActors() if obj.image != None]
 
-        for actor in sorted(actors, key=lambda actor: actor.zIndex):
-            baseImage = assets.getImage(actor.image)
-            image = pygame.transform.scale(baseImage, (actor.scale.x, actor.scale.y))
-            rect = image.get_rect()
-            rect.x = actor.position.x
-            rect.y = actor.position.y
+        for actor in sorted(actors, key=lambda actor : actor.zIndex):
+            imgSurface = assets.getImage(actor.image)
+            if imgSurface:
+                if not actor.surface:
+                    actor.surface = imgSurface
 
-            if actor.rotation != 0:
-                image = pygame.transform.rotate(image, -actor.rotation).convert_alpha()
-            rect = image.get_rect(center=image.get_rect(center = (actor.position.x, actor.position.y)).center)
+                # scale
+                if actor.surface.get_size() != (actor.scale.x, actor.scale.y):
+                    actor.surface = pygame.transform.scale(imgSurface, (actor.scale.x, actor.scale.y))
+                rect = actor.surface.get_rect()
+                rect.x, rect.y = actor.position.x, actor.position.y
 
-            if image:
-                if actor.color != (255,255,255):
-                    image.fill(actor.color, special_flags=pygame.BLEND_RGB_MULT)
-                image.set_colorkey((255, 0, 255))
-                surface.blit(image, rect)
+                # rotation
+                finalSurface = actor.surface
+                if actor.rotation != 0:
+                    finalSurface = pygame.transform.rotate(finalSurface, -actor.rotation)
+
+                rect = finalSurface.get_rect(center=actor.surface.get_rect(center=(actor.position.x, actor.position.y)).center)
+                finalSurface.set_colorkey((255, 0, 255))
+                surface.blit(finalSurface, rect)
